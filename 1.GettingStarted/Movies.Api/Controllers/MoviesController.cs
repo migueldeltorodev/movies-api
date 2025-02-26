e744cd747c10a3a78220705f110e090283190647
Miguel David González del Toro <migueldgdt@gmail.com>
2025-02-25 21:23:46 -0500

    refactor(MoviesController): updated the controller's endpoints

---
+++ 1.GettingStarted/Movies.Api/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Movies.Api.Mapping;
+using Movies.Application.Repositories;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Controllers;
+
+[ApiController]
+public class MoviesController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MoviesController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    [HttpPost(ApiEndpoints.Movies.Create)]
+    public async Task<IActionResult> Create([FromBody] CreateMovieRequest request)
+    {
+        var movie = request.MapToMovie();
+        await _movieRepository.CreateAsync(movie);
+        return Created($"/{ApiEndpoints.Movies.Create}/{movie.Id}", movie);
+    }
+}
