7006da84306d5c2c1549a8fbccafc30d79a36056
Miguel David González del Toro <migueldgdt@gmail.com>
2025-03-02 10:44:50 -0500

    refactor(MovieService): added repo and modified the update service

--- 1.GettingStarted/Movies.Application/ApplicationServiceCollectionExtensions.cs
+++ 1.GettingStarted/Movies.Application/ApplicationServiceCollectionExtensions.cs
@@ -12,6 +12,7 @@ public static class ApplicationServiceCollectionExtensions
     {
         services.AddSingleton<IMovieRepository, MovieRepository>();
         services.AddSingleton<IMovieService, MovieService>();
+        services.AddSingleton<IRatingRepository, RatingRepository>();
         services.AddValidatorsFromAssemblyContaining<IApplicationMarker>(ServiceLifetime.Singleton);
         return services;
     }
--- 1.GettingStarted/Movies.Application/Services/MovieService.cs
+++ 1.GettingStarted/Movies.Application/Services/MovieService.cs
@@ -8,11 +8,13 @@ public class MovieService : IMovieService
 {
     private readonly IMovieRepository _movieRepository;
     private readonly IValidator<Movie> _movieValidator;
+    private readonly IRatingRepository _ratingRepository;
 
-    public MovieService(IMovieRepository movieRepository, IValidator<Movie> movieValidator)
+    public MovieService(IMovieRepository movieRepository, IValidator<Movie> movieValidator, IRatingRepository ratingRepository)
     {
         _movieRepository = movieRepository;
         _movieValidator = movieValidator;
+        _ratingRepository = ratingRepository;
     }
 
     public async Task<bool> CreateAsync(Movie movie, CancellationToken cancellationToken = default)
@@ -46,7 +48,18 @@ public class MovieService : IMovieService
             return null;
         }
         
-        await _movieRepository.UpdateAsync(movie, userId, cancellationToken);
+        await _movieRepository.UpdateAsync(movie, cancellationToken);
+
+        if (!userId.HasValue)
+        {
+            var rating = await _ratingRepository.GetRatingAsync(movie.Id, cancellationToken);
+            movie.Rating = rating;
+            return movie;
+        }
+        
+        var ratings = await _ratingRepository.GetRatingAsync(movie.Id, userId.Value, cancellationToken);
+        movie.Rating = ratings.Rating;
+        movie.UserRating = ratings.UserRating;
         return movie;
     }
 
