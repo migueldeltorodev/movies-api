f7d65198a6840aea23d76069c4be958ba4b92a95
Miguel David González del Toro <migueldgdt@gmail.com>
2025-03-02 10:43:04 -0500

    build(RatingRepo): created basic reting repo

---
+++ 1.GettingStarted/Movies.Application/Repositories/RatingRepository.cs
@@ -0,0 +1,39 @@
+using System.Data;
+using Dapper;
+using Movies.Application.Database;
+
+namespace Movies.Application.Repositories;
+
+public class RatingRepository : IRatingRepository
+{
+    private readonly IDbConnectionFactory _dbConnectionFactory;
+
+    public RatingRepository(IDbConnectionFactory dbConnectionFactory)
+    {
+        _dbConnectionFactory = dbConnectionFactory;
+    }
+
+    public async Task<float?> GetRatingAsync(Guid movieId, CancellationToken cancellationToken = default)
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync(cancellationToken);
+        return await connection.QuerySingleOrDefaultAsync<float?>(new CommandDefinition("""
+               select round(avg(r.rating), 1) from ratings r
+               where movieid = @movieId
+               """, new { movieId }, cancellationToken: cancellationToken));
+    }
+
+    public async Task<(float? Rating, int? UserRating)> GetRatingAsync(Guid movieId, Guid userId, CancellationToken cancellationToken = default)
+    {
+        using var connection = await _dbConnectionFactory.CreateConnectionAsync(cancellationToken);
+        return await connection.QuerySingleOrDefaultAsync<(float?, int?)>(new CommandDefinition("""
+            select round(avg(r.rating), 1),
+                   (select rating
+                    from ratings
+                    where movieid = @movieId
+                    and userid = @userId
+                    limit 1)
+            from ratings r
+            where movieid = @movieId
+            """, new { movieId, userId }, cancellationToken: cancellationToken));
+    }
+}
