39b4f75d9564a3683b5810ae5858d89cafb45784
Miguel David González del Toro <migueldgdt@gmail.com>
2025-07-12 21:48:36 -0400

    feat: created contracts for auth and register

--- 1.GettingStarted/Movies.Api/Controllers/V1/RatingsController.cs
+++ 1.GettingStarted/Movies.Api/Controllers/V1/RatingsController.cs
@@ -1,58 +1,58 @@
-using Asp.Versioning;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Movies.Api.Auth;
-using Movies.Api.Mapping;
-using Movies.Application.Services;
-using Movies.Contracts.Requests;
-using Movies.Contracts.Responses;
-
-namespace Movies.Api.Controllers.V1;
-
-[ApiController]
-[ApiVersion(1.0)]
-public class RatingsController : ControllerBase
-{
-    private readonly IRatingService _ratingService;
-
-    public RatingsController(IRatingService ratingService)
-    {
-        _ratingService = ratingService;
-    }
-
-    [Authorize]
-    [HttpPut(ApiEndpoints.Movies.Rate)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> RateMovie([FromRoute]Guid id, 
-        [FromBody]RateMovieRequest request, 
-        CancellationToken cancellationToken = default)
-    {
-        var userId = HttpContext.GetUserId();
-        var result = await _ratingService.RateMovieAsync(id, request.Rating, userId!.Value, cancellationToken);
-        return result ? Ok() : NotFound();
-    }
-
-    [Authorize]
-    [HttpDelete(ApiEndpoints.Movies.DeleteRating)]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> DeleteRating([FromRoute] Guid id,
-        CancellationToken cancellationToken = default)
-    {
-        var userId = HttpContext.GetUserId();
-        var result = await _ratingService.DeleteRatingAsync(id, userId!.Value, cancellationToken);
-        return result ? Ok() : NotFound();
-    }
-
-    [Authorize]
-    [HttpGet(ApiEndpoints.Ratings.GetUserRatings)]
-    [ProducesResponseType(typeof(IEnumerable<MovieRatingResponse>), StatusCodes.Status200OK)]
-    public async Task<IActionResult> GetUserRatings(CancellationToken cancellationToken = default)
-    {
-        var userId = HttpContext.GetUserId();
-        var ratings = await _ratingService.GetRatingsForUserAsync(userId!.Value, cancellationToken);
-        var ratingsResponse = ratings.MapToResponse();
-        return Ok(ratingsResponse);
-    }
-}
+// using Asp.Versioning;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Mvc;
+// using Movies.Api.Auth;
+// using Movies.Api.Mapping;
+// using Movies.Application.Services;
+// using Movies.Contracts.Requests;
+// using Movies.Contracts.Responses;
+//
+// namespace Movies.Api.Controllers.V1;
+//
+// [ApiController]
+// [ApiVersion(1.0)]
+// public class RatingsController : ControllerBase
+// {
+//     private readonly IRatingService _ratingService;
+//
+//     public RatingsController(IRatingService ratingService)
+//     {
+//         _ratingService = ratingService;
+//     }
+//
+//     [Authorize]
+//     [HttpPut(ApiEndpoints.Movies.Rate)]
+//     [ProducesResponseType(StatusCodes.Status200OK)]
+//     [ProducesResponseType(StatusCodes.Status404NotFound)]
+//     public async Task<IActionResult> RateMovie([FromRoute]Guid id, 
+//         [FromBody]RateMovieRequest request, 
+//         CancellationToken cancellationToken = default)
+//     {
+//         var userId = HttpContext.GetUserId();
+//         var result = await _ratingService.RateMovieAsync(id, request.Rating, userId!.Value, cancellationToken);
+//         return result ? Ok() : NotFound();
+//     }
+//
+//     [Authorize]
+//     [HttpDelete(ApiEndpoints.Movies.DeleteRating)]
+//     [ProducesResponseType(StatusCodes.Status200OK)]
+//     [ProducesResponseType(StatusCodes.Status404NotFound)]
+//     public async Task<IActionResult> DeleteRating([FromRoute] Guid id,
+//         CancellationToken cancellationToken = default)
+//     {
+//         var userId = HttpContext.GetUserId();
+//         var result = await _ratingService.DeleteRatingAsync(id, userId!.Value, cancellationToken);
+//         return result ? Ok() : NotFound();
+//     }
+//
+//     [Authorize]
+//     [HttpGet(ApiEndpoints.Ratings.GetUserRatings)]
+//     [ProducesResponseType(typeof(IEnumerable<MovieRatingResponse>), StatusCodes.Status200OK)]
+//     public async Task<IActionResult> GetUserRatings(CancellationToken cancellationToken = default)
+//     {
+//         var userId = HttpContext.GetUserId();
+//         var ratings = await _ratingService.GetRatingsForUserAsync(userId!.Value, cancellationToken);
+//         var ratingsResponse = ratings.MapToResponse();
+//         return Ok(ratingsResponse);
+//     }
+// }
